7b0197f8c9ee9684979102821de46cd411e67ec7
roottool <smart.dog.lilienthal@gmail.com>
2018-11-18 17:33:09 +0900

    Updates

---
+++ precache-manifest.4e9b7b12e08ae2b60d5fa08b2e7ad737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ad6855517754593a06c509f9ebf8db1",
+    "url": "/static/media/EchoCat.5ad68555.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "22872c29d02efd26026a",
+    "url": "/static/js/main.22872c29.chunk.js"
+  },
+  {
+    "revision": "9dcfa4e06b3b4ac312a7",
+    "url": "/static/js/1.9dcfa4e0.chunk.js"
+  },
+  {
+    "revision": "22872c29d02efd26026a",
+    "url": "/static/css/main.51023e11.chunk.css"
+  },
+  {
+    "revision": "fec9f452f57217ed79124d00c3e91bdb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b89920ae91eb4e723ac9e96893a92aff.js"
+  "/precache-manifest.4e9b7b12e08ae2b60d5fa08b2e7ad737.js"
 );
 
 workbox.clientsClaim();
